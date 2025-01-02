892dcbcac38b38c5e18c42aa83f99c0f45895d45
Ashutosh0624 <ashutosh0602phd@gmail.com>
2025-01-02 12:44:04 +0530

    updated code

--- Factory/dogs_cats_factory.py
+++ Factory/dogs_cats_factory.py
@@ -6,10 +6,20 @@ class Animal(ABC):
         pass
 
 class Dog(Animal):
+    ''' A simple dog class '''
+    def __init__(self, name):
+        self._name = name 
+
+    def speak(self):
+        return "woof"
+
     def animal_type(self):
         return "Dog"
 
 class Cat(Animal):
+    def __init__(self, name):
+        self._name = name 
+
     def animal_type(self):
         return "Cat"
     
@@ -17,9 +27,9 @@ class AnimalFactory:
     @staticmethod
     def animal_type(species_type):
         if species_type == "dog":
-            return Dog()
+            return Dog('sheru')
         elif species_type == "cat":
-            return Cat()
+            return Cat('willi')
         else:
             raise ValueError("Invalid species type")
 
@@ -27,3 +37,4 @@ if __name__ == "__main__":
     animal = AnimalFactory()
     pet_type = animal.animal_type('dog')
     print(pet_type.animal_type())
+    print(pet_type._name)
